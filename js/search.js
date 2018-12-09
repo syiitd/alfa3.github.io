9f50bd9b1be5445c8cf5a3b59926f8db981d2123
alfa3 <34172709+syiitd@users.noreply.github.com>
2018-12-09 18:17:01 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
-(function() {
-  var cx = '008845399276725189042:garndlp46bg';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
+
+  (function() {
+    var cx = '002069176679973668637:wz-pivkotyy';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
   })();
